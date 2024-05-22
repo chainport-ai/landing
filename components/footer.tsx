660375bfc740f44a62a6a8d1d3e83669e3b4ef36
jotyy <jotyy318@gmail.com>
2024-05-22 10:03:14 +0800

    chore: update footer and logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,7 @@
 
 import { siteConfig } from "@/lib/config";
 import { MountainSnow } from "lucide-react";
+import Image from "next/image";
 
 export const Footer = () => {
   return (
@@ -9,7 +10,12 @@ export const Footer = () => {
       <div className="md:px-12 lg:px-28">
         <div className="container m-auto space-y-6 text-gray-600 dark:text-gray-300">
           <div className="flex items-center space-x-4 mx-auto justify-center">
-            <MountainSnow className="w-12 h-12" />
+            <Image
+              src={siteConfig.global.logo}
+              alt={siteConfig.global.name}
+              width={48}
+              height={48}
+            />
             <h2 className="font-display text-2xl">{siteConfig.global.name}</h2>
           </div>
           <ul
@@ -35,7 +41,26 @@ export const Footer = () => {
                 <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"></path>
               </svg>
             </a>
-            <a href="#" title="facebook" target="blank" aria-label="facebook">
+            {/* phone */}
+            <a href="https://github.com/Chain-Port" aria-label="github">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="w-5 h-5"
+              >
+                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                <path d="M9 18c-4.51 2-5-2-7-2" />
+              </svg>
+            </a>
+
+            {/* <a href="#" title="facebook" target="blank" aria-label="facebook">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -54,12 +79,12 @@ export const Footer = () => {
               >
                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"></path>
               </svg>
-            </a>
+            </a> */}
           </div>
 
           <div className="text-center">
             <span className="text-sm tracking-wide">
-              Copyright © Chainport <span id="year">2024</span> | All right
+              Copyright © Chain Port LLC <span id="year">2024</span> | All right
               reserved
             </span>
           </div>
